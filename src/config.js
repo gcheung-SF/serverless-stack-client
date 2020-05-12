8f3f30ab910dcc79537eabfaeb63fb8b8eccba20
Gloria Cheung <gcheung@stanford.edu>
2020-05-12 09:44:20 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "gc-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://eqo78bavdf.execute-api.us-west-2.amazonaws.com/gc"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_JLjSqfuwn",
+    APP_CLIENT_ID: "43fgls7n78o8e7l952j8iorjom",
+    IDENTITY_POOL_ID: "us-west-2:617ffcb7-5601-4ddf-9aec-cfa89b3dccb9"
+  }
+};
